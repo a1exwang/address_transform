cd10d0b03e4f61ec32c7a0142b996a4c61f30a3b
Alex Wang <ice_b0und@hotmail.com>
2016-03-09 17:01:21 +0800

    add command line

---
+++ transform.rb
@@ -0,0 +1,30 @@
+open("out.txt", "r") do |f|
+	@memory = f.read.split
+end
+
+#puts @memory
+
+@cr3 = 544
+@addr = ARGV.count == 1 ? ARGV[0] : 0x6c74
+
+pde_index = (@addr & 0x7c00) >> 10
+pte_index = (@addr & 0x3e0) >> 5
+offset = @addr & 0x1f
+
+pde = @memory[@cr3 + pde_index].to_i(16)
+if pde < 0x80
+	puts 'invalid pde'
+	exit
+end
+
+ptep = (pde - 0x80) << 5
+pte = @memory[ptep + pte_index].to_i(16)
+if pte < 0x80
+	puts 'invalid pte'
+	exit
+end
+
+start = (pte - 0x80) << 5
+addr = start + offset
+content = @memory[addr]
+puts addr: addr, content: content
